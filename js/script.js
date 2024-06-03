c557e753125be8e06096729a9e9ee408de48117d
WesSno <kofibaafik@gmail.com>
2024-06-03 18:10:18 +0000

    updated CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 const tooltip = document.querySelector(".tooltip");
 const shareIcon = document.querySelector(".share-icon");
+const mobileShare = document.querySelector(".mobile-tooltip i");
+const mobileSocialMedia = document.querySelector(".social-media");
 
-tooltip.addEventListener("mouseenter", () => {
+function showTooltip() {
   tooltip.classList.add("visible");
-  shareIcon.classList.add("rounded");
-});
+  mobileSocialMedia.classList.add("visible_two");
+}
 
-tooltip.addEventListener("mouseleave", () => {
+function hideTooltip() {
   tooltip.classList.remove("visible");
-  shareIcon.classList.remove("rounded");
-});
+  mobileSocialMedia.classList.remove("visible_two");
+}
+
+tooltip.addEventListener("mouseenter", showTooltip);
+tooltip.addEventListener("mouseleave", hideTooltip);
+tooltip.addEventListener("touchstart", showTooltip);
+tooltip.addEventListener("touchend", hideTooltip);
+
+mobileShare.addEventListener("mouseenter", showTooltip);
+mobileShare.addEventListener("mouseleave", hideTooltip);
+mobileShare.addEventListener("touchstart", showTooltip);
+mobileShare.addEventListener("touchend", hideTooltip);
