5ee6894a1fb0843dd25f1576c6e0718984cf7135
WesSno <kofibaafik@gmail.com>
2024-06-05 16:11:18 +0000

    updated script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ const tooltip = document.querySelector(".tooltip");
 const shareIcon = document.querySelector(".share-icon");
 const mobileShare = document.querySelector(".mobile-tooltip i");
 const mobileSocialMedia = document.querySelector(".social-media");
+const rootStyles = getComputedStyle(document.documentElement);
+const desaturatedDarkBlue = rootStyles
+  .getPropertyValue("--desaturated-dark-blue")
+  .trim();
+const lightGrayishBlue = rootStyles
+  .getPropertyValue("--light-grayish-blue")
+  .trim();
 
 function showTooltip() {
   tooltip.classList.add("visible");
@@ -15,10 +22,34 @@ function hideTooltip() {
 
 tooltip.addEventListener("mouseenter", showTooltip);
 tooltip.addEventListener("mouseleave", hideTooltip);
-tooltip.addEventListener("touchstart", showTooltip);
-tooltip.addEventListener("touchend", hideTooltip);
+tooltip.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  showTooltip();
+  tooltip.style.backgroundColor = desaturatedDarkBlue;
+  tooltip.style.color = "white";
+});
+tooltip.addEventListener("touchend", (e) => {
+  e.preventDefault();
+  setTimeout(() => {
+    hideTooltip();
+    tooltip.style.color = desaturatedDarkBlue;
+    tooltip.style.backgroundColor = lightGrayishBlue;
+  }, 2000);
+});
 
 mobileShare.addEventListener("mouseenter", showTooltip);
 mobileShare.addEventListener("mouseleave", hideTooltip);
-mobileShare.addEventListener("touchstart", showTooltip);
-mobileShare.addEventListener("touchend", hideTooltip);
+mobileShare.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  showTooltip();
+  mobileShare.style.backgroundColor = desaturatedDarkBlue;
+  mobileShare.style.color = "white";
+});
+mobileShare.addEventListener("touchend", (e) => {
+  e.preventDefault();
+  setTimeout(() => {
+    hideTooltip();
+    mobileShare.style.color = desaturatedDarkBlue;
+    mobileShare.style.backgroundColor = lightGrayishBlue;
+  }, 2000);
+});
